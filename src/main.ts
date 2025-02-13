aefe6b40763c60d8fd436c3d079edb3ea5687f4c
hisusqristos <varyadaser@gmail.com>
2025-02-13 15:04:29 +0400

    now it crops, but esim vonc

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,64 @@
-const input: string = '<input type="file" name="file" id="file">'
-const inputFile = document.getElementById("file")
-// VIEW
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h3>takiye perogi</h3>
-      ${input}
-      <form method="post" action="">
-      <input type="file" id="image_input" accept="image/jpeg, image/png, image/jpg">
-      <input id="modified_image" name="image" type="hidden" value="">
-      <input type="submit" value="Upload">
-      </form>
   </div>`
+
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d')!;
+if (!ctx) throw new Error("I cannot work with this canvas thing you gave me");
+
+let startX = 0;
+let startY = 0;
+let endX = 0;
+let endY = 0;
+let isSelecting = false;
+
+const img = new Image();
+img.src = "./public/23006218-3-Brown-bear-in-lupine-meadow.jpg";
+
+img.onload = () => {
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+};
+
+img.onerror = (err) => {
+  console.error("Failed to load image", err);
+};
+
+canvas.addEventListener('mousedown', (e) => {
+  const insideCanvas =
+    e.offsetX >= 0 &&
+    e.offsetX <= canvas.width &&
+    e.offsetY >= 0 &&
+    e.offsetY <= canvas.height;
+
+  if (!insideCanvas) return;
+  isSelecting = true;
+  startX = e.offsetX;
+  startY = e.offsetY;
+});
+
+canvas.addEventListener('mousemove', (mouse) => {
+  if (!isSelecting) return;
+  endX = mouse.offsetX;
+  endY = mouse.offsetY;
+
+
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = 'red';
+  ctx.strokeRect(startX, startY, endX - startX, endY - startY);
+});
+
+canvas.addEventListener('mouseup', () => {
+  isSelecting = false;
+  if ((endX < 10) && (endY < 10)) return;
+
+  const cropWidth = endX - startX;
+  const cropHeight = endY - startY;
+
+  console.log("x, y and widths", startX, startY, cropWidth, cropHeight, "\n Start and end", startX, startY, endX, endY)
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, startX, startY, cropWidth, cropHeight,
+    0, 0, cropWidth, cropHeight)
+})
